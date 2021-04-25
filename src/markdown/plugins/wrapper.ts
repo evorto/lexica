74c72afa697440702fea9bd79de168d86bf110b0
Lukas Nehrke <lukasnehrke@protonmail.com>
2021-04-25 14:17:55 +0200

    Remove debug statements

--- src/markdown/plugins/wrapper.ts
+++ src/markdown/plugins/wrapper.ts
@@ -1,9 +1,7 @@
 import * as Markdown from "markdown-it";
 
 export default (md: Markdown): void => {
-  console.log('init')
   md.renderer.rules.table_open = function () {
-    console.log('opened!')
     return '<div class="table-wrapper"><table class="table">\n';
   };
   md.renderer.rules.table_close = function () {
